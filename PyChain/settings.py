a63a6f3aab140ea03cb9f605525bd4dfa81c13fb
SurabhiMore <surabhi.pmore@gmail.com>
2021-04-22 14:40:51 +0530

    initial commit

--- PyChain/settings.py
+++ PyChain/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '^s)o14p$auv!401k(2(kv5=+ma*lajaab@rr%ksddaff-%7ug)'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PyChain.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -116,7 +111,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
@@ -124,9 +118,5 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-#CORS_ORIGIN_WHITELIST = [
-         #'http://127.0.0.1:3000',
-         #]
-#CORS_ORIGIN_REGEX_WHITELIST = [
-#    'http://127.0.0.1:3000',
-#]
+# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000']
+# CORS_ORIGIN_REGEX_WHITELIST = ['http://127.0.0.1:3000']
